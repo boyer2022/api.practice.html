be20777e10fec322338563c4a85c5c981e5c9ad9
boyer2022 <m.boyer2016@outlook.com>
2025-02-27 11:16:32 -0600

    Changed result to picture for clarity.

---
+++ results.js
@@ -0,0 +1,9 @@
+const cat_result = document.getElementById("cat_result");
+const dog_result = document.getElementById("dog_result");
+const cat_btn = document.getElementById("cat_btn");
+const dog_btn = document.getElementById("dog_btn");
+
+cat_btn.addEventListener("click", getRandomCat);
+dog_btn.addEventListener("click", getRandomdog);
+
+function getRandomCat() {}
